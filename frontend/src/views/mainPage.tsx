0ac54b116ccc2814370b8ebdfbd425ffce7536be
0ddodd <hegoeson@gmail.com>
2025-03-23 21:32:58 +0900

    contact api 생성

--- frontend/src/views/mainPage.tsx
+++ frontend/src/views/mainPage.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InputForm from '../components/inputForm'
+import axios from 'axios';
+
+interface InfoItem {
+    country_nm: string;
+    country_eng_nm: string;
+    contact_remark: string;
+    flag_download_url: string;
+    dang_map_download_url: string;
+    [key: string]: string;
+}
 
 function MainPage () {
+
+    const [infoItem, setInfoItem] = useState<InfoItem | null>(null);
+
+    useEffect(() => {
+        console.log(import.meta.env.VITE_API_BACKEND_URL)
+        getData();
+    }, []);
+
+    const getData = async () => {
+        // 실험용
+        try {
+            const res = await axios.get(`${import.meta.env.VITE_API_BACKEND_URL}/contact`);
+            console.log(res.data.msg);
+            setInfoItem(res.data.items.item[0]);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div>
-            <InputForm />
+            {infoItem && <div>
+                <h2>{infoItem.country_nm}</h2>
+                <img src={infoItem.flag_download_url} alt="country flag" />
+                <h3>{infoItem.country_eng_nm}</h3>
+                <div dangerouslySetInnerHTML={{ __html: infoItem.contact_remark }} />
+                <img src={infoItem.dang_map_download_url} alt="" />
+            </div>}
+            {/* <InputForm /> */}
         </div>
     )
 }
--- frontend/src/views/postPage.tsx
+++ frontend/src/views/postPage.tsx
@@ -1,73 +1,65 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 function postPage() {
-    const [posts, setPosts] = useState<{id:number, title: string, content: string}[]>([]);
-    const [editMode, setEditMode] = useState({
-        id: undefined,
-        mode: false
-    });
+    const [posts, setPosts] = useState<{id: number, title: string, content: string}[]>
+    ([]);
+    const [editMode, setEditMode] = useState({id: undefined, mode: false});
     const [newTitle, setNewTitle] = useState("");
     const [newContent, setNewContent] = useState("");
 
-    useEffect(() => {
-        // 로컬 스토리지에서 글 목록 받아오기
-        const existedposts = JSON.parse(localStorage.getItem('posts') || "[]")
-        setPosts(existedposts);
-    }, []);
+    useEffect(()=>{
+        const parsedPosts = JSON.parse(localStorage.getItem('posts')) || [];
+        console.log(parsedPosts);    
+        setPosts(parsedPosts);
+    },[]);
 
     const handleUpdateMode = (id: number) => {
+        const editPost = posts.find(post => post.id === id); 
         setEditMode({id, mode: true});
+        setNewTitle(editPost?.title);
+        setNewContent(editPost?.content);
     };
 
     const handleUpdatePost = (id: number) => {
-        const updatedPosts = posts.map(post => post.id === id ? {...post, title: newTitle, content: newContent} : post);
+        const updatedPosts = posts.map(post => post.id === id 
+            ? {...post, title: newTitle, content: newContent} : post);
         setPosts(updatedPosts);
+        setEditMode({id, mode: false});
         localStorage.setItem("posts", JSON.stringify(updatedPosts));
-        setEditMode({id: undefined, mode: false});
-    }
+    };
 
     const handleDeletePost = (id: number) => {
         const leftPosts = posts.filter(post => post.id !== id);
         setPosts(leftPosts);
         localStorage.setItem("posts", JSON.stringify(leftPosts));
-    };
+    }
 
     return (
         <div>
-            <ul>
-                {posts && posts.length === 0 ? (
-                    <p>저장된 글이 없습니다.</p>
-                ) : (
-                    posts.map((post:{id: number, title: string, content: string}) => (
-                        <li key={post.id}>
-                            <div>
-                                {editMode.mode && editMode.id === post.id ?
-                                <>
-                                    <input
-                                        value={newTitle}
-                                        onChange={(e)=>setNewTitle(e.target.value)}
-                                    />
-                                    <textarea
-                                        value={newContent}
-                                        onChange={(e)=>setNewContent(e.target.value)}
-                                    />
-                                    <button onClick={()=>handleUpdatePost(post.id)}>완료</button>
-                                </>
-                                :
-                                <>
-                                    <p>{post.title}</p>
-                                    <p>{post.content}</p>
-                                <button onClick={()=>handleUpdateMode(post.id)}>수정</button>
-                                    <button onClick={()=>handleDeletePost(post.id)}>
-                                        삭제
-                                    </button>
-                                </>
-                                }
-                            </div>
-                        </li>
-                    ))
-                )}
-            </ul>
+            {posts.map(post => 
+                <div key={post.id}>
+                    {editMode.mode && post.id === editMode.id ?
+                        <>
+                            <input
+                                value={newTitle}
+                                onChange={(e)=>setNewTitle(e.target.value)}
+                            />
+                            <textarea
+                                value={newContent}
+                                onChange={(e)=>setNewContent(e.target.value)}
+                            />
+                            <button onClick={()=>handleUpdatePost(post.id)}>완료</button>
+                        </>
+                    :
+                        <>
+                            <p>{post.title}</p>
+                            <p>{post.content}</p>
+                            <button onClick={()=>handleUpdateMode(post.id)}>수정</button>
+                            <button onClick={()=>handleDeletePost(post.id)}>삭제</button>
+                        </>
+                    }
+                </div>
+            )}
         </div>
     )
 }
