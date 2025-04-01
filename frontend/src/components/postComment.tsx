11436b9fbce6f14c5d224ae1af1ef359d6e3facf
0ddodd <hegoeson@gmail.com>
2025-04-02 01:36:11 +0900

    date

--- frontend/src/components/postComment.tsx
+++ frontend/src/components/postComment.tsx
@@ -4,14 +4,19 @@ import React, { useEffect, useState } from 'react'
 function PostComment({id, post}) {
 
     const [comment, setComment] = useState<string>("");
-    const [comments, setComments] = useState<{comment: string, _id: string}[]>([]);
+    const [comments, setComments] = useState<{
+        comment: string, createdAt: Date, _id: string
+    }[]>([]);
+    const [newComment, setNewComment] = useState<string>("");
+    const [isEditMode, setIsEditMode] = useState(
+        {id: '', mode: false}
+    );
     
     const onSubmitComment = async () => {
         if (!comment.trim()) return;
         const newComments = [...comments, {comment}];
 
         try {
-            console.log(newComments);
             const resp = await axios.patch(`${import.meta.env.VITE_API_BACKEND_URL}/post/${id}`, 
                 {comments: newComments}
             );
@@ -29,11 +34,33 @@ function PostComment({id, post}) {
 
     const handleDeleteComment = async (commentId: string) => {
         try {
-            const newComments = comments.filter(comment => comment._id !== commentId);
+            const newComments = comments.filter(com => com._id !== commentId);
             const resp = await axios.patch(`${import.meta.env.VITE_API_BACKEND_URL}/post/${id}`, 
                 {comments: newComments}
             );
             setComments(resp.data.post.comments);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const onEditMode = async (commentId: string, text: string) => {
+        setNewComment(text);
+        setIsEditMode({id: commentId, mode: true});
+    };
+
+    const handleUpdateComment = async (commentId: string) => {
+        try {
+            const newComments = comments.map(com => com._id === commentId
+                ? {...com, comment: newComment, createdAt: Date.now()}
+                : com
+            );
+            const resp = await axios.patch(`${import.meta.env.VITE_API_BACKEND_URL}/post/${id}`, 
+                {comments: newComments}
+            );
+            console.log(resp);
+            setComments(resp.data.post.comments);
+            setIsEditMode({id, mode: false});
         } catch (err) {
             console.error(err);
         }
@@ -54,8 +81,25 @@ function PostComment({id, post}) {
             <div>
                 {comments && comments.length > 0 && comments.map(c => 
                     <div key={c._id}>
-                        <span>{c.comment}</span>
-                        <button>수정</button>
+                        {
+                            isEditMode.id === c._id && isEditMode.mode ?
+                            <>
+                                <input
+                                    type="text"
+                                    value={newComment}
+                                    onChange={(e) => setNewComment(e.target.value)}
+                                />
+                                <button
+                                    onClick={()=>handleUpdateComment(c._id)}
+                                >완료</button>
+                            </>
+                            :
+                            <>
+                                <span>{c.comment}</span>
+                                <button onClick={()=>onEditMode(c._id, c.comment)}>수정</button>
+                            </>
+                        }
+                        <span>{new Date(c.createdAt).toLocaleString()}</span>
                         <button onClick={()=>handleDeleteComment(c._id)}>삭제</button>
                     </div>
                 )}
